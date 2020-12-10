0322f212daa2945bb61c3fe671d8992f2f9aa1a1
Gomez <tonatiuh.gomez@hp.com>
2020-12-09 20:35:07 -0600

    fixing bugs

--- template/index.php
+++ template/index.php
@@ -4,6 +4,8 @@ if(!isset($_SESSION["user_param"]))
 { 
 	require_once('../admin/main_fan.php');
 } else{
-	require_once('../admin/main_loginin.php');
+	echo ("<SCRIPT LANGUAGE='JavaScript'>
+            window.location.href='../comolovemifan/';
+            </SCRIPT>");
 }
 ?>
