24662e4bf876ab8920ce83fe57a0a8e04945a2f2
Gomez <tonatiuh.gomez@hp.com>
2020-12-09 20:41:57 -0600

    fixing bugs 2.0

--- comolovemifan/index.php
+++ comolovemifan/index.php
@@ -1,3 +1,11 @@
 <?php
-    require_once('../admin/main_fan.php');
+session_start(); 
+if(isset($_SESSION["user_param"])) 
+{ 
+	require_once('../admin/main_fan.php');
+} else{
+	echo ("<SCRIPT LANGUAGE='JavaScript'>
+            window.location.href='../';
+            </SCRIPT>");
+}
 ?>
