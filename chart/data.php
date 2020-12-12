6482aaf510ca2475bd66629783f13b7603c599db
Gomez <tonatiuh.gomez@hp.com>
2020-12-12 00:33:58 -0600

    Update data.php

--- chart/data.php
+++ chart/data.php
@@ -1,10 +1,10 @@
 <?php
 header('Content-Type: application/json');
-
+session_start();
 require_once('../admin/cn.php');
 
 //$sqlQuery = "SELECT student_id,student_name,marks FROM tbl_marks ORDER BY student_id";
-$sqlQuery = "SELECT DATE(date) as x, COUNT(*) as y FROM visitors_users WHERE id_user=5 AND date >= date_sub(curdate(), interval 7 day) GROUP BY DAY(date)";
+$sqlQuery = "SELECT DATE(date) as x, COUNT(*) as y FROM visitors_users WHERE id_user=".$_SESSION["user_param"]." AND date >= date_sub(curdate(), interval 7 day) GROUP BY DAY(date)";
 
 $result = mysqli_query($conn,$sqlQuery);
 
